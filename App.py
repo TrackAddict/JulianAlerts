0b85edf1b0f75001793b600943bb07075f06b528
TrackAddict <mjhatch@ucsd.edu>
2016-10-14 01:15:49 +0000

    Landing text update

--- App.py
+++ App.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 @app.route("/", methods=['GET', 'POST'])
 def hello_monkey():
 	resp = twilio.twiml.Response()
-	resp.say("Hello Monkey")
+	resp.say("This will eventually become Julian Alerts. Please stay tuned for updates.")
 
 	return str(resp)
 
